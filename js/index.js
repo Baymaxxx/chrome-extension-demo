6ef20c8da305a51b21393bfcd1b1b36700f2f15c
baymaxxx <81243232@qq.com>
2018-03-28 14:12:44 +0800

    add 意大利炮

---
+++ js/index.js
@@ -0,0 +1,115 @@
+/*
+ * Encoding   : UTF-8
+ * Description: chrome拓展 屏蔽百度推广链接
+ *
+ * @author    @齐士博 <qii404@126.com>
+ *
+ * @link http://weibo.com/shiboooo/
+ */
+
+var qii404 = {
+  /*
+     * 右侧推荐开关设置
+     */
+  rightSwitch: 1,
+
+  /*
+     * 初始化
+     */
+  init: function() {
+    this.initRightSwitch()
+    this.bindAction()
+    this.removeAds()
+  },
+
+  /*
+     * 初始化右侧推荐开关
+     */
+  initRightSwitch: function() {
+    var this_ = this
+    chrome.storage.sync.get('right_ad_switch', function(data) {
+      this_.rightSwitch = data['right_ad_switch']
+      this_.removeRightAds()
+    })
+  },
+
+  /*
+     * 清理
+     */
+  removeAds: function() {
+    // this.removeNormalAds();
+    this.removeMockAds()
+    this.removeRightAds()
+  },
+
+  /*
+     * 删除通用形式广告
+     */
+  removeNormalAds: function() {
+    console.log('start removing normal ads...')
+
+    var ads = document.querySelectorAll(
+      '#content_left>div:not([class*="result"]):not([class="leftBlock"])'
+    )
+
+    for (var i = 0; i < ads.length; i++) {
+      ads[i].remove()
+      console.log(ads[i])
+    }
+  },
+
+  /*
+     * 删除一些假冒是搜索结果的特殊广告
+     */
+  removeMockAds: function() {
+    console.log('start removing mock ads...')
+    var ads = document.querySelectorAll('#content_left>div')
+
+    for (var i = 0; i < ads.length; i++) {
+      var ms = ads[i].querySelectorAll('span')
+      for (var j = 0; j < ms.length; j++) {
+        if (ms[j].innerHTML === '广告') {
+          ads[i].remove()
+
+          console.log(ads[i])
+          break
+        }
+      }
+    }
+  },
+
+  /*
+     * 删除右侧【广告？ or 推荐？】
+     */
+  removeRightAds: function() {
+    var rightElement = document.querySelector('#content_right')
+
+    if (rightElement) {
+      if (this.rightSwitch != 1) {
+        // rightElement.style.display='none';
+        rightElement.remove()
+      }
+    }
+  },
+
+  /*
+     * 绑定
+     */
+  bindAction: function() {
+    var this_ = this
+    var observer = new MutationObserver(function() {
+      this_.removeAds()
+    })
+
+    observer.observe(document.querySelector('#wrapper_wrapper'), {
+      childList: true,
+      characterData: false,
+      attributes: false,
+      subtree: true
+    })
+  }
+}
+
+qii404.init()
+
+// end of file Hide.js
